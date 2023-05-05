c442ca663c1ae38eeea406d98e1fe65f65f451f1
Szymek131 <sz.wiktor131@gmail.com>
2023-05-06 01:16:55 +0200

    few improvements

--- app/script.js
+++ app/script.js
@@ -1,47 +1,65 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { render } from 'react-dom';
 
+const statuses = {
+  work: "work",
+  rest: "rest",
+  off: "off"
+}
+
+const workTime = 1200;
+const restTime = 20;
+
+const formatTime = time => {
+  let m = Math.floor(time / 60)
+  let s = (time - (m * 60))
+  if (s < 10) {
+    s = '0' + s;
+  }
+  if (m < 10) {
+    m = '0' + m;
+  }
+  const convertedTime = m + ':' + s;
+  return convertedTime;
+}
+
+const playBell = () => {
+  const bell = new Audio('./sounds/bell.wav');
+  bell.play();
+};
+
 const App = () => {
-  const [status, setStatus] = useState('off')
+  const [status, setStatus] = useState(statuses.off)
   const [time, setTime] = useState(0)
   const [timer, setTimer] = useState(null)
 
-  const formatTime = time => {
-    let m = Math.floor(time / 60)
-    let s = (time - (m * 60))
-    if (s < 10) {
-      s = '0' + s;
+  useEffect(() => {
+    if (time === 0) {
+      if (status === statuses.work) {
+        playBell();
+        setStatus(statuses.rest)
+        setTime(restTime);
+      } else if (status === statuses.rest) {
+        playBell();
+        setStatus(statuses.work)
+        setTime(workTime);
+      }
     }
-    if (m < 10) {
-      m = '0' + m;
-    }
-    const convertedTime = (m) + ':' + s;
-    return convertedTime;
-  }
-
-  if (time === 0) {
-    if (status === 'work') {
-      setStatus('rest')
-      setTime(20);
-    } else if (status === 'rest') {
-      setStatus('work')
-      setTime(1200);
-    }
-  }
+  });
 
   const startTimer = () => {
-    setTime(1200);
-    setStatus('work');
+    setTime(workTime);
+    setStatus(statuses.work);
     setTimer(setInterval(() => {
       setTime(time => time - 1);
-    }, 1000));
+    }, 1));
   }
 
   const stopTimer = () => {
-    setTimer(null);
+    setTimer(clearInterval);
     setTime(0);
-    setStatus('off');
+    setStatus(statuses.off);
   }
 
   const closeApp = () => {
@@ -51,15 +69,15 @@ const App = () => {
   return (
     <div>
       <h1>Protect your eyes</h1>
-      {status === 'off' && <p>According to optometrists in order to save your eyes, you should follow the 20/20/20. It means you should to rest your eyes every 20 minutes for 20 seconds by looking more than 20 feet away.</p>}
-      {status === 'off' && <p>This app will help you track your time and inform you when it's time to rest.</p>}
-      {status === 'work' && <img src="./images/work.png" />}
-      {status === 'rest' && <img src="./images/rest.png" />}
-      {status !== 'off' && <div className="timer">
+      {status === statuses.off && <p>According to optometrists in order to save your eyes, you should follow the 20/20/20. It means you should to rest your eyes every 20 minutes for 20 seconds by looking more than 20 feet away.</p>}
+      {status === statuses.off && <p>This app will help you track your time and inform you when it's time to rest.</p>}
+      {status === statuses.work && <img src="./images/work.png" />}
+      {status === statuses.rest && <img src="./images/rest.png" />}
+      {status !== statuses.off && <div className="timer">
         {formatTime(time)}
       </div>}
-      {status === 'off' && <button className="btn" onClick={startTimer}>Start</button>}
-      {status !== 'off' && <button className="btn" onClick={stopTimer}>Stop</button>}
+      {status === statuses.off && <button className="btn" onClick={startTimer}>Start</button>}
+      {status !== statuses.off && <button className="btn" onClick={stopTimer}>Stop</button>}
       <button className="btn btn-close" onClick={closeApp}>X</button>
     </div>
   )
